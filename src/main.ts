39334c5b3d455ec9e85c26ad24e6c94fb9bf99c0
David <davidgall@ext.inditex.com>
2026-06-15 13:34:48 +0200

    feat: add TypeScript concepts and examples

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './style.css'
+//import './bases/01-const-let' 
+//import './bases/02-template-let' 
+//import './bases/03-object-literal' 
+//import './bases/04-arrays' 
+//import './bases/05-functions' 
+//import './bases/06-obj-destructuring' 
+import './bases/07-array-destructuring' 
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div class="hero">
+    <h1>Hola Mundo</h1>
+  </div>
+</section>
+
+<div class="ticks"></div>
+
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
